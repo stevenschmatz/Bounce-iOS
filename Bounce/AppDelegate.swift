03fa44f0e4f74a9b49a43a5eb7942d3a7ed6a6cd
Steven Schmatz <stevenschmatz@gmail.com>
2014-12-22 14:03:13 -0500

    Added Crashlytics

--- Bounce/AppDelegate.swift
+++ Bounce/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreData
 import Fabric
 import TwitterKit
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        Fabric.with([Twitter()])
+        Fabric.with([Twitter(), Crashlytics()])
         
         // Override point for customization after application launch.
         return true

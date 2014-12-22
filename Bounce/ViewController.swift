afdd6f10ba25e3dc65cc1dc089b907e2c4bb1aa5
Steven Schmatz <stevenschmatz@gmail.com>
2014-12-22 13:16:53 -0500

    Initial commit

---
+++ Bounce/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bounce
+//
+//  Created by Steven on 12/22/14.
+//  Copyright (c) 2014 stevenschmatz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

e1a987e24f99dee737ef89ca578ead32a6804b5c
rockinbinbin <Robin@Robins-Air.home>
2015-01-02 14:37:39 -0500

    padding added to fields + buttons

---
+++ Bounce/ChatTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ChatTableViewCell.m
+//  Bounce
+//
+//  Created by Robin Mehta on 1/2/15.
+//  Copyright (c) 2015 stevenschmatz. All rights reserved.
+//
+
+#import "ChatTableViewCell.h"
+
+@implementation ChatTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ ChatTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatTableViewController.h
+//  Bounce
+//
+//  Created by Robin Mehta on 1/2/15.
+//  Copyright (c) 2015 stevenschmatz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatTableViewController : UITableViewController
+
+@end

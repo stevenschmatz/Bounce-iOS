cf3c1c98b0a7628c121351dc7fbd23d4b235a36a
Steven Schmatz <stevenschmatz@gmail.com>
2015-01-01 23:18:04 -0500

    Changed status bar color to white

--- Bounce/AppDelegate.swift
+++ Bounce/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         Fabric.with([Twitter(), Crashlytics()])
         self.customizeNavBar()
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
         
         // Override point for customization after application launch.
         return true
